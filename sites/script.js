b6608f1acbd7c44f4491ad867113a48273aefb80
naz22 <139713840+Naziz2@users.noreply.github.com>
2024-02-11 19:30:25 +0100

    script.js

--- sites/script.js
+++ sites/script.js
@@ -1,45 +1,39 @@
 let toggleBtn = document.getElementById('toggle-btn');
+let nom=document.getElementsByClassName("footer");
 let body = document.body;
 let darkMode = localStorage.getItem('dark-mode');
 
-const enableDarkMode = () =>{
-   toggleBtn.classList.replace('fa-sun', 'fa-moon');
+const enableDarkMode = () => {
    body.classList.add('dark');
    localStorage.setItem('dark-mode', 'enabled');
-}
+};
 
-const disableDarkMode = () =>{
-   toggleBtn.classList.replace('fa-moon', 'fa-sun');
+const disableDarkMode = () => {
    body.classList.remove('dark');
    localStorage.setItem('dark-mode', 'disabled');
-}
+};
 
-if(darkMode === 'enabled'){
+if (darkMode === 'enabled') {
    enableDarkMode();
 }
 
-toggleBtn.onclick = (e) =>{
+toggleBtn.onclick = () => {
    darkMode = localStorage.getItem('dark-mode');
-   if(darkMode === 'disabled'){
+   if (darkMode === 'disabled') {
       enableDarkMode();
-   }else{
+   } else {
       disableDarkMode();
    }
-}
+};
+
 
 let profile = document.querySelector('.header .flex .profile');
 
 document.querySelector('#user-btn').onclick = () =>{
    profile.classList.toggle('active');
-   search.classList.remove('active');
 }
 
-let search = document.querySelector('.header .flex .search-form');
 
-document.querySelector('#search-btn').onclick = () =>{
-   search.classList.toggle('active');
-   profile.classList.remove('active');
-}
 
 let sideBar = document.querySelector('.side-bar');
 
@@ -48,17 +42,4 @@ document.querySelector('#menu-btn').onclick = () =>{
    body.classList.toggle('active');
 }
 
-document.querySelector('#close-btn').onclick = () =>{
-   sideBar.classList.remove('active');
-   body.classList.remove('active');
-}
 
-window.onscroll = () =>{
-   profile.classList.remove('active');
-   search.classList.remove('active');
-
-   if(window.innerWidth < 1200){
-      sideBar.classList.remove('active');
-      body.classList.remove('active');
-   }
-}
